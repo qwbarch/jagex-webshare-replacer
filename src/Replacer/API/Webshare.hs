eb3936fd0fd403cd7e94ae036887623c8d0fcba0
qwbarch <qwbarch@gmail.com>
2026-02-18 15:31:41 -0500

    Add replacement endpoints

--- src/Replacer/API/Webshare.hs
+++ src/Replacer/API/Webshare.hs
@@ -2,16 +2,20 @@ module Replacer.API.Webshare where
 
 import qualified Data.ByteString.Char8 as ByteString
 import Control.Monad.Reader
+import Data.Function
 import Data.Text
 import Data.String.Interpolate
 import Data.Aeson
 import Data.Aeson.TH
+import Data.Aeson.QQ.Simple
 import Data.Maybe
+import Data.List.NonEmpty
 import Network.HTTP.Req
 import Replacer.Env
 import Replacer.Config
 import Replacer.Request
 import Replacer.Proxy
+import UnliftIO.Exception
 
 base = https "proxy.webshare.io" /: "api"
 
@@ -20,6 +24,26 @@ newtype DownloadTokenResponse = DownloadTokenResponse
   deriving Show
 
 deriveJSON (defaultOptions { fieldLabelModifier = camelTo2 '_' }) ''DownloadTokenResponse
+
+newtype ReplacementId = ReplacementId Text
+  deriving newtype Show
+
+deriveJSON defaultOptions ''ReplacementId
+
+newtype CreateReplacementResponse = CreateReplacementResponse
+  { id :: ReplacementId }
+  deriving Show
+
+deriveJSON defaultOptions ''CreateReplacementResponse
+
+data GetReplacementResponse = GetReplacementResponse
+  { state :: Text
+  , proxiesAdded :: Int
+  , error :: Maybe Text
+  , errorCode :: Maybe Text
+  }
+
+deriveJSON (defaultOptions { fieldLabelModifier = camelTo2 '_' }) ''GetReplacementResponse
 
 requestWebshare body scheme response method url = do
   apiKey <- asks (.config.apiKey)
@@ -53,3 +77,25 @@ getProxies planId = do
             . ByteString.split '\n'
             . ByteString.filter (/= '\r')
   parseProxies <$> requestWebshare NoReqBody query bsResponse GET url
+
+createReplacement planId addresses = do
+  config <- asks (.config)
+  let url = base /: "v3" /: "proxy" /: "replace"
+      query = "plan_id" =: (planId :: Int)
+      requestBody =
+        object
+          [ "to_replace" .= object
+              [ "type" .= ("ip_address" :: String)
+              , "ip_addresses" .= addresses
+              ]
+          , "replace_with" .= config.replaceWith
+          , "dry_run" .= False
+          ]
+  response :: CreateReplacementResponse <- requestWebshare (ReqBodyJson requestBody) query jsonResponse POST url
+  pure $ response.id
+
+getReplacement planId replacementId = do
+  config <- asks (.config)
+  let url = base /: "v3" /: "proxy" /: "replace" /: replacementId
+      query = "plan_id" =: (planId :: Int)
+  requestWebshare NoReqBody query (jsonResponse @GetReplacementResponse) POST url
--- src/Replacer/Config.hs
+++ src/Replacer/Config.hs
@@ -4,29 +4,34 @@
 module Replacer.Config where
 
 import qualified Data.ByteString.Lazy as ByteString
-import Control.Exception 
 import Control.Monad
 import Control.Monad.IO.Class
 import Control.Monad.Extra
 import Data.Aeson
 import Data.Aeson.TH
 import Data.Aeson.Encode.Pretty (encodePretty)
+import Data.Aeson.QQ.Simple
 import Data.Text (Text)
 import Data.Typeable
 import System.Directory
+import UnliftIO.Exception
 
 configPath = "config.json"
 
 data Config = Config
   { apiKey :: Text
   , planIds :: [Int]
+  , maxThreads :: Int
+  , replaceWith :: Value
   }
 
-deriveJSON defaultOptions ''Config
+deriveJSON (defaultOptions { fieldLabelModifier = camelTo2 '_' }) ''Config
 
 defaultConfig = Config
   { apiKey = "YOUR_API_KEY_HERE"
   , planIds = []
+  , maxThreads = 30
+  , replaceWith = [aesonQQ|[{"type": "country", "country_code": "US"}]|]
   }
 
 data ConfigError = ConfigError String
